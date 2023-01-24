c0ae9150d48b615f1b6d797254c18584eeab0afe
Ryo Kurosawa <90682107+shiro-kur@users.noreply.github.com>
2023-01-24 13:17:10 +0900

    Add customed-script SpliceAI

---
+++ Customed_SpliceAI/__main__for_customed_SpliceAI.py
@@ -0,0 +1,83 @@
+import sys
+import argparse
+import logging
+import pysam
+from spliceai.utils import Annotator, get_delta_scores
+
+
+try:
+    from sys.stdin import buffer as std_in
+    from sys.stdout import buffer as std_out
+except ImportError:
+    from sys import stdin as std_in
+    from sys import stdout as std_out
+
+
+def get_options():
+
+    parser = argparse.ArgumentParser(description='Version: 1.3')
+    parser.add_argument('-I', metavar='input', nargs='?', default=std_in,
+                        help='path to the input VCF file, defaults to standard in')
+    parser.add_argument('-O', metavar='output', nargs='?', default=std_out,
+                        help='path to the output VCF file, defaults to standard out')
+    parser.add_argument('-R', metavar='reference', required=True,
+                        help='path to the reference genome fasta file')
+    parser.add_argument('-A', metavar='annotation', required=True,
+                        help='"grch37" (GENCODE V24lift37 canonical annotation file in '
+                             'package), "grch38" (GENCODE V24 canonical annotation file in '
+                             'package), or path to a similar custom gene annotation file')
+    parser.add_argument('-D', metavar='distance', nargs='?', default=50,
+                        type=int, choices=range(0, 5000),
+                        help='maximum distance between the variant and gained/lost splice '
+                             'site, defaults to 50')
+    parser.add_argument('-M', metavar='mask', nargs='?', default=0,
+                        type=int, choices=[0, 1],
+                        help='mask scores representing annotated acceptor/donor gain and '
+                             'unannotated acceptor/donor loss, defaults to 0')
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+
+    args = get_options()
+
+    if None in [args.I, args.O, args.D, args.M]:
+        logging.error('Usage: spliceai [-h] [-I [input]] [-O [output]] -R reference -A annotation '
+                      '[-D [distance]] [-M [mask]]')
+        exit()
+
+    try:
+        vcf = pysam.VariantFile(args.I)
+    except (IOError, ValueError) as e:
+        logging.error('{}'.format(e))
+        exit()
+
+    header = vcf.header
+    header.add_line('##INFO=<ID=SpliceAI,Number=.,Type=String,Description="SpliceAIv1.3.1 variant '
+                    'annotation.(output-modified ver by Kurosawa, et al. 2023) '
+                    'These include delta scores (DS) and raw scores (RS) and delta positions (DP) '
+                    'for acceptor gain (AG), acceptor loss (AL), donor gain (DG), and donor loss (DL). '
+                    'Format: ALLELE|GENE_ID|DS_AG|RS_AG|DP_AG|DS_AL|RS_AL|DP_AL|DS_DG|RS_DG|DP_DG|DS_DL|RS_DL|DP_DL"> ')
+
+    try:
+        output = pysam.VariantFile(args.O, mode='w', header=header)
+    except (IOError, ValueError) as e:
+        logging.error('{}'.format(e))
+        exit()
+
+    ann = Annotator(args.R, args.A)
+
+    for record in vcf:
+        scores = get_delta_scores(record, ann, args.D, args.M)
+        if len(scores) > 0:
+            record.info['SpliceAI'] = scores
+        output.write(record)
+
+    vcf.close()
+    output.close()
+
+
+if __name__ == '__main__':
+    main()
